f5a1d527f98432f3afa37a5e8873ff021795abf7
Derrickmv <frederrick.villanueva@yahoo.com>
2016-07-19 10:32:18 -0700

    main method in class Program comments

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,6 +10,8 @@ namespace Lab2
     {
         /// <summary>
         /// The main entry point for the application.
+        /// Main method that enables app visual styles and sets comptaible text rendering defaults
+        /// then it goes ahead and runs the Form application.
         /// </summary>
         [STAThread]
         static void Main()
